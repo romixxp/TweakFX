2b2d59197337f855af69fc0ba119af82f0a13141
romixxp <108482106+romixxp@users.noreply.github.com>
2025-06-07 16:09:45 +0300

    Update _cvars.cs

--- TweakFX/core/_cvars.cs
+++ TweakFX/core/_cvars.cs
@@ -2,6 +2,8 @@ namespace TweakFX.core
 {
     public static class _cvars
     {
-        public static string ASIO_name = "Focusrite USB ASIO"
+        public static string ASIO_name = "Focusrite USB ASIO";
+        public static int ASIO_buffersize = 192;
+        public static int ASIO_initsamplerate = 44100; 
     }
 }
